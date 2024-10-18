5414ebcf00f946b11d832c029822944d2ee58a55
artkl12 <arta.klauze@gmail.com>
2024-10-18 13:13:26 +0300

    commit

--- index.js
+++ index.js
@@ -34,6 +34,7 @@ app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :body")
 );
 app.use(cors())
+app.use(express.static('dist'))
 
 app.get("/", (request, response) => {
   response.send("<h1>Hello World!</h1>");
